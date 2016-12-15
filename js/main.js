5076fa616a9fe327f2dab14470f3f1e150611fbc
sergey <sergey>
2016-12-15 17:20:03 +0300

    v2

--- js/main.js
+++ js/main.js
@@ -1,22 +1,81 @@
+ $(document).ready(function(){
 
-  $(document).ready(function () {
+  $(".b-accordion__content").not(":first").hide();
 
-    $('.b-accordion>.b-accordion__content').not(':first').hide();
+  $(".b-accordion__title").click(function(){
+    var findContent = $(this).next(".b-accordion__content");
+    var findAccordion = $(this).closest('.b-accordion');
+    
+    if($(this).hasClass("b-accordion__title_active")){
+      $(".b-accordion__title").removeClass("b-accordion__title_active");
+    }else{
+      $(".b-accordion__title").removeClass("b-accordion__title_active");
+      $(this).addClass("b-accordion__title_active");
+    }
 
-    $('.b-accordion>.b-accordion__title').click(function () {
+    
+    if (findContent.is(":visible")){
+      findContent.slideUp(400);
 
-      var findContent = $(this).next('.b-accordion__content');
-      var findAccordion = $(this).closest('.b-accordion');
+    }else{
 
-      if(findContent.is(':visible')){
-        findAccordion.slideUp(400);
-      }else{
-        findAccordion.find('>.b-accordion__content').slideUp(400);
-        findContent.slideDown(400);
-      }
-
-    });
-
-
-
+      findAccordion.find(".b-accordion__content").slideUp(400);
+      findContent.slideDown(400);
+    }
   });
+
+ });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ // $(document).ready(function () {
+
+  //   $('.b-accordion>.b-accordion__content').not(':first').hide();
+
+  //   $('.b-accordion>.b-accordion__title').click(function () {
+
+  //     var findContent = $(this).next('.b-accordion__content');
+  //     var findAccordion = $(this).closest('.b-accordion');
+
+  //     if(findContent.is(':visible')){
+  //       findAccordion.slideUp(400);
+  //     }else{
+  //       findAccordion.find('>.b-accordion__content').slideUp(400);
+  //       findContent.slideDown(400);
+  //     }
+
+  //   });
+
+
+
+  // });
